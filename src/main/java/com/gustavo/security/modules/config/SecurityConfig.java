a40d55bbad02d08ed14d275ca32289f02d6bdcd2
gustas01 <gustavohdlima@hotmail.com>
2023-07-31 14:59:17 -0300

    Feat: Habilitanto CORS

--- src/main/java/com/gustavo/security/modules/config/SecurityConfig.java
+++ src/main/java/com/gustavo/security/modules/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,7 +25,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
-        return http.csrf(csrf -> csrf.disable())
+        return http.cors(Customizer.withDefaults()).csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
                     .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
